5b180174f1bc338b84ff5aba6d3c0f0599ab47ee
icemao <648558423@qq.com>
2023-07-31 09:30:46 +0800

    code by myself

--- Code/section_5_2.py
+++ Code/section_5_2.py
@@ -1,5 +1,7 @@
 import numpy as np
-import 5-1
+from matplotlib import pyplot as plt
+from tqdm import tqdm  # tqdm是显示循环进度条的库
+from Code.section_5_1 import CliffWalkingEnv
 
 class nstep_Sarsa:
     """ n步Sarsa算法 """
@@ -51,3 +53,49 @@ class nstep_Sarsa:
             self.state_list = []
             self.action_list = []
             self.reward_list = []
+
+np.random.seed(0)
+ncol = 12
+nrow = 4
+env = CliffWalkingEnv(ncol, nrow)
+
+n_step = 5  # 5步Sarsa算法
+alpha = 0.1
+epsilon = 0.1
+gamma = 0.9
+agent = nstep_Sarsa(n_step, ncol, nrow, epsilon, alpha, gamma)
+num_episodes = 500  # 智能体在环境中运行的序列的数量
+
+return_list = []  # 记录每一条序列的回报
+for i in range(10):  # 显示10个进度条
+    #tqdm的进度条功能
+    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+        for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
+            episode_return = 0
+            state = env.reset()
+            action = agent.take_action(state)
+            done = False
+            while not done:
+                next_state, reward, done = env.step(action)
+                next_action = agent.take_action(next_state)
+                episode_return += reward  # 这里回报的计算不进行折扣因子衰减
+                agent.update(state, action, reward, next_state, next_action,
+                             done)
+                state = next_state
+                action = next_action
+            return_list.append(episode_return)
+            if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
+                pbar.set_postfix({
+                    'episode':
+                    '%d' % (num_episodes / 10 * i + i_episode + 1),
+                    'return':
+                    '%.3f' % np.mean(return_list[-10:])
+                })
+            pbar.update(1)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('5-step Sarsa on {}'.format('Cliff Walking'))
+plt.show()
